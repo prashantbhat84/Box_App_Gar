ac8a4a686bd027adab0c057c4c259247a311d7b1
Prashant Bhat <prashantbhat84@gmail.com>
2021-07-12 10:13:17 +0530

    changes to test CD

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const dotenv = require('dotenv')
 dotenv.config({ path: "./config/config.env" });
 const port = process.env.PORT;
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server  on port ${port}`);
 })
