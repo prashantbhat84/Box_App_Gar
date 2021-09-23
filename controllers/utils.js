4603c61d2756d6d5e727b59b908e29c4fd57498a
Prashant Bhat <prashantbhat84@gmail.com>
2021-09-23 13:31:11 +0530

    UTILS Module
    modified  the secret generation code using actual box id

--- controllers/utils.js
+++ controllers/utils.js
@@ -1,20 +1,18 @@
 
 const BoxModel = require('../models/box')
 
-const getConvertedVal = (input) => {
 
-    return ("0x" + input.charCodeAt(0).toString(16))
-}
 const getHmacKeyArray = (input, arraylength) => {
     const hmac = [];
 
     for (let i = 0; i < arraylength - 1; i++) {
-        const hexval = getConvertedVal(input[i])
-        const hexval1 = getConvertedVal(input[i + 1]);
+        const hexval = (input[i])
+        const hexval1 = (input[i + 1]);
         const hmacVal = (hexval ^ hexval1) | 0x63
         hmac.push(hmacVal.toString())
     }
-    const lastmacval = getConvertedVal(input[arraylength - 1])
+
+    const lastmacval = (input[arraylength - 1])
     const hamcresult = (lastmacval ^ 0x79) | 0x63;
     hmac.push(hamcresult.toString())
     return hmac;
@@ -23,42 +21,52 @@ const getHmacKeyArray = (input, arraylength) => {
 const getEncryptedAESKey = (input, arraylength) => {
     const aeskey = []
     for (let i = 0; i < arraylength; i++) {
-        const hexval = getConvertedVal(input[i])
+
+        const hexval = (input[i])
         let val = (hexval | 0x47).toString()
-        aeskey.push(val);
+        aeskey.push(val)
     }
     for (let i = 8; i < 15; i++) {
-        const hexval = getConvertedVal(input[i - 8])
-        const hexval1 = getConvertedVal(input[i - 7])
+        const hexval = (input[i - 8])
+
+        const hexval1 = (input[i - 7])
+
         const result = ((hexval) ^ (hexval1)) | 0x47
         aeskey.push(result.toString())
     }
 
-    const hexval2 = getConvertedVal(input[7])
+    const hexval2 = (input[7])
+
     const res1 = ((hexval2) ^ 0x79) | 0x47
     aeskey.push(res1.toString())
     return aeskey
 }
 class Utils {
-  
-    constructor () {
-        if ( !Utils.instance ) {
+
+    constructor() {
+        if (!Utils.instance) {
             Utils.instance = this;
         }
         return Utils.instance;
     }
 
     getSecret(boxid) {
-      
+
         const str = boxid
-        const strarray1 = Array.from(str);
-        const arraylength = strarray1.length;
-        if (arraylength === 16) {
-            const hmac = getHmacKeyArray(strarray1, arraylength)
-            const aeskey = getEncryptedAESKey(strarray1, arraylength);
+        const arr2 = [];
+        const arr = Array.from(str);
+        for (i = 0; i < arr.length;) {
+            let var2 = '0x' + arr[i] + arr[i + 1];
+            arr2.push(var2);
+            i = i + 2
+        }
+        const arraylength = arr2.length;
+        if (arraylength === 8) {
+            const hmac = getHmacKeyArray(arr2, arraylength)
+            const aeskey = getEncryptedAESKey(arr2, arraylength);
             return { hmac, aeskey }
         } else {
-            throw new Error("Boxid length should be 8")
+            throw new Error("Boxid length should be 16")
         }
 
     }
@@ -99,4 +107,4 @@ const utils = new Utils();
 
 Object.freeze(utils)
 
-module.exports =utils
+module.exports = utils
