c4752e8ecff6cddef345cbfe549f61334fc42690
Prashant Bhat <prashantbhat84@gmail.com>
2021-09-23 13:49:48 +0530

    Aggregator and Box Update
    created spaces for better readability

--- controllers/aggregator.js
+++ controllers/aggregator.js
@@ -168,11 +168,17 @@ class Aggregator {
             log.info({ module: "AggregatorAnd Box Update" }, details)
             const message = commandMessage + "," + lidStatusMessage + " " + "&" + motionStatus + " " + `on ${details.date}`;
             log.info({ module: "Aggregator And Box Update" }, `Date: ${details.date}`)
+          
             log.info({ module: "Aggregator And Box Update" }, `BoxID: ${details.box}`)
+
             log.info({ module: "Aggregator And Box Update" }, `AggID: ${details.aggid}`);
+
             log.info({ module: "Aggregator And Box Update" }, `BoxLid: ${lidStatusMessage}`)
+
             log.info({ module: "Aggregator And Box Update" }, `Motion: ${motionStatus}`)
+
             log.info({ module: "Aggregator And Box Update" }, `Temperature: ${details.temperature}`);
+            
             log.info({ module: "Aggregator And Box Update" }, `Command: ${commandMessage}`);
             const boxDetails = await BoxModel.findOne({ boxid: details.box });
            
